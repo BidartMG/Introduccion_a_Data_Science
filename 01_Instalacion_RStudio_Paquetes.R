96c43e2e8ca55796446d3ad73b07d94fc6888bb4
Monica G. Bidart <bidartmgb@hotmail.com>
2020-09-25 17:15:53 -0300

    Instalacion_RStudio_Paquetes

---
+++ 01_Instalacion_RStudio_Paquetes.R
@@ -0,0 +1,14 @@
+# En la consola escribir
+install.packages(c("ggplot2","swirl","lattice","car","fImport","tseries"))
+
+library("ggplot2")
+library(swirl)
+library("lattice")
+library(car)
+library("fImport")
+library("tseries")
+
+# car pedí este paquete
+install.packages("carData")
+
+
